aaeb05aaa8c376540e37b43f31eba8de990b7516
Nairen Fu <nairenf2@illinois.edu>
2021-02-26 16:03:08 -0600

    Updated Course to allow blank section_titles and migrated.

--- electives/courses/models.py
+++ electives/courses/models.py
@@ -4,7 +4,7 @@ class Course(models.Model):
     # Unique ID, Major, Code, SectionTitle e.g. CS##498001
     uid = models.CharField(max_length = 10, unique = True, primary_key = True)
     name = models.CharField(max_length = 50)
-    section_title = models.CharField(max_length = 300, unique = True)
+    section_title = models.CharField(max_length = 300, unique = True, blank = True, null = True)
     description = models.TextField()
 
 class Link(models.Model):
@@ -13,6 +13,6 @@ class Link(models.Model):
         Course, related_name = 'fulfilments', on_delete = models.CASCADE
     )
     target = models.ForeignKey(
-        Course, related_name = 'prerequisites', on_delete=models.CASCADE
+        Course, related_name = 'prerequisites', on_delete = models.CASCADE
     )
 
