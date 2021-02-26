d5b1f91ac578debe7644cb1b6b9e05a6fc163108
Nairen Fu <nairenf2@illinois.edu>
2021-02-26 15:43:52 -0600

    Implemented Course and Link models, and migrated them.

--- electives/courses/models.py
+++ electives/courses/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    # Unique ID, Major, Code, SectionTitle e.g. CS##498001
+    uid = models.CharField(max_length = 10, unique = True, primary_key = True)
+    name = models.CharField(max_length = 50)
+    section_title = models.CharField(max_length = 300, unique = True)
+    description = models.TextField()
+
+class Link(models.Model):
+    pid = models.IntegerField()
+    prerequisite = models.ForeignKey(
+        Course, related_name = 'fulfilments', on_delete = models.CASCADE
+    )
+    target = models.ForeignKey(
+        Course, related_name = 'prerequisites', on_delete=models.CASCADE
+    )
+
