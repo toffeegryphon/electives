5f732b2db6111cfa211818ec5874e9f82f012ec2
Nairen Fu <nairenf2@illinois.edu>
2021-02-26 14:40:36 -0600

    Set up migrations, database name and timezone.

---
+++ electives/courses/urls.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index')
+]
+
--- electives/electives/settings.py
+++ electives/electives/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'electives.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'courses.sqlite3',
     }
 }
 
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
