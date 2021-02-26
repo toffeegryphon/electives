bd80145645dc69dc29caf9f75159d9b59dc679ee
Nairen Fu <nairenf2@illinois.edu>
2021-02-26 16:35:29 -0600

    Decoupled and regenerated secret key.

--- electives/electives/settings.py
+++ electives/electives/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ff4xt70*=corz)=syw$)r^xkl+x04v$*iyuvwdg)w$3d-2l)w@'
+SECRET_KEY = environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
