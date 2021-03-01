dc5751af31c1ac7e51867eb2f19b92d99f0e9a36
Nairen Fu <nairenf2@illinois.edu>
2021-02-28 18:39:20 -0600

    Changed implementation of serializers to reflect grouping.

--- electives/courses/serializers.py
+++ electives/courses/serializers.py
@@ -4,17 +4,32 @@
 from .models import Course, Link
 from rest_framework import serializers
 
-class PrerequisiteField(serializers.RelatedField):
-    # TODO Consider how to serialize
-    # Probably JSON of uid, string, and link
-    def to_representation(self, link: Link):
-        p = link.prerequisite
+class PrerequisiteSerializer(serializers.ModelSerializer):
+    prerequisites = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Link
+        fields = ['prerequisites']
+
+    def get_prerequisites(self, obj):
+        # TODO Consider how to serialize
+        # Probably JSON of uid, string, and link:
+        p = obj.prerequisite
         return {'uid': p.uid, 'str': str(p)}
 
-
 class CourseSerializer(serializers.ModelSerializer):
-    prerequisites = PrerequisiteField(many=True, read_only=True)
+    prerequisites = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_prerequisites(self, obj):
+        links = obj.prerequisites.all()
+        tree = {}
+        # TODO Probably should be doing some prefetching
+        for link in links:
+            if link.pid not in tree:
+                tree[link.pid] = []
+            tree[link.pid].append(PrerequisiteSerializer(link).data)
+        return tree.values()
