7ef286314a296282f100b814cf08f563d3cca761
Nairen Fu <nairenf2@illinois.edu>
2021-03-14 21:36:49 -0500

    Implemented list vs detail view.

--- prerequisites/src/App.js
+++ prerequisites/src/App.js
@@ -6,16 +6,43 @@ import { getCourse } from './Course/actions.js'
 
 import './App.css'
 
+const TYPE_LIST = 0
+const TYPE_DETAIL = 1
+
 export default class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { course: {} }
+    this.state = {
+      type: TYPE_LIST,
+      courses: [],
+      meta: {},
+      course: {} 
+    }
+  }
+
+  _buildMeta = (data) => {
+    return {
+      // Should be called total, count for current size
+      count: data.count,
+      next: data.next,
+      previous: data.previous
+    }
   }
 
   search = async (query) => {
     try {
       const response = await getCourse(query)
-      this.setState({ course: response.data })
+      const data = response.data
+      if ('count' in data) {
+        // TODO should do merging
+        this.setState({ 
+          type: TYPE_LIST, 
+          courses: data.results, 
+          meta: this._buildMeta(data)
+        })
+      } else {
+        this.setState({ type: TYPE_DETAIL, course: response.data })
+      }
     } catch (err) {
       // TODO Use axios.interceptors
       if (err.response.status === 404) {
@@ -26,10 +53,24 @@ export default class App extends Component {
   }
 
   render() {
+    let content
+    switch (this.state.type) {
+      case TYPE_LIST:
+        content = []
+        for (const course of this.state.courses) {
+          content.push(<Course course={course}/>)
+        }
+        break
+      case TYPE_DETAIL:
+        content = <Course course={this.state.course}/>
+        break
+      default:
+        break
+    }
     return (
       <div className="App">
         <Search search={this.search}/>
-        <Course course={this.state.course}/>
+        {content}
       </div>
     )
   }
